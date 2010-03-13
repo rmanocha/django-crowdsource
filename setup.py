21449a388ab63785d2124ad97cce484251e6b3e5
Rishabh Manocha <rmanocha@gmail.com>
2010-03-13 14:16:39 +0800

    Adding a setup.py so that django_crowdsource can be installed via pip.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="django-crowdsource",
+    version="0.1",
+    description="An app to allow users to enter urls for custom defined fields.",
+    author="Rishabh Manocha",
+    author_email="rmanocha@gmail.com",
+    url="http://github.com/rmanocha/django-crowdsource",
+    packages=["crowdsource"],
+)
